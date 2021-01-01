50c2ac577c4e63357806f421bb5ace2455c452d0
Varun Veerbhadra <54136118+varunveeraa@users.noreply.github.com>
2021-01-01 18:32:25 +0530

    Update face_detection.py

--- face_detection.py
+++ face_detection.py
@@ -3,11 +3,11 @@
 #importing the library
 import cv2
 
-#loading the cascades
+#loading the classifier
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
-# Defining a function which does detections
+#defining a function which does detections
 def detect(gray, frame):
     faces = face_cascade.detectMultiScale(gray, 1.3, 10)
     for (x, y, w, h) in faces:
@@ -19,12 +19,13 @@ def detect(gray, frame):
             cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
     return frame
 
-#Face detection through Webcam
+#face detection through Webcam
 video_capture = cv2.VideoCapture(0)
 while True:
     _, frame = video_capture.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    canvas = detect(gray, frame)
+    canvas = detect(gray, frame) #calling the detect function
+#previewing session:
     cv2.imshow('Video', canvas)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
